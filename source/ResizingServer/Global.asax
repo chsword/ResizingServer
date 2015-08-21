d8ef60323691e6c44ceadc647e54acaeecb28fac
Zou Jian <chsword@126.com>
2015-08-21 15:14:22 +0800

    init

---
+++ source/ResizingServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ResizingServer.WebApiApplication" Language="C#" %>
---
+++ source/ResizingServer/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Routing;
+
+namespace ResizingServer
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+        }
+    }
+}
